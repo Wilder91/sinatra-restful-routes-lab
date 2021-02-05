d91dd938d0e5bd14936343c26e1f524e47524bbc
Mike Wilder <mikewilder01@gmail.com>
2021-02-05 18:11:06 -0500

    commit tree 70787a4e29491c6c239f2e956e6200bacaf9b7ae

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,53 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
+  get '/' do
+    redirect to '/recipes'
+  end
+
+  get '/recipes' do 
+    @recipes = Recipe.all
+    erb :index
+  end
+
+  get '/recipes/new' do # loads form to create new recipe
+    erb :new
+  end
+
+  post '/recipes' do # create action creating and saving new recipe based on params from form, then redirects to show page
+    @recipe = Recipe.create(params)
+
+    redirect to "/recipes/#{@recipe.id}"
+  end
+
+  get '/recipes/:id' do # show action to display a single recipe. dynamic url allows ID to be called in the view thru params hash
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :show
+  end
+
+  get '/recipes/:id/edit' do #load edit form
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :edit
+  end
+
+  patch '/recipes/:id' do #edit action. edit form submission. redirect to show page.
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
+    @recipe.save
+
+    redirect to "/recipes/#{@recipe.id}"
+  end
+
+  delete '/recipes/:id' do #delete action
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.delete
+
+    redirect to '/recipes'
+  end
+end 
+
   # code actions here!
 
-end
+
